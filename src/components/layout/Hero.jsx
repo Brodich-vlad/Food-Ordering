79b176e4925da9446b867d4e738ffc21b0e52b3a
Brodich Vlad <brodich_vlad@ukr.net>
2026-03-03 21:58:39 +0200

    VB:create project

---
+++ src/components/layout/Hero.jsx
@@ -0,0 +1,15 @@
+export default function Hero(){
+  return(
+    <section>
+      <h1 className="text-4xl font-semibold">   
+        Everything<br />
+        is better<br />
+        with a&nbsp;
+        <span className="text-red-500">
+            Pizza
+          </span></h1>
+      <p>Pizza is the missing piece that makes every day complete, a simple yet delicious joy in life</p>
+
+    </section>
+  )
+}
