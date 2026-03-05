24db57cb5c6bac3b14085f94e34254185af05e7d
Brodich Vlad <brodich_vlad@ukr.net>
2026-03-05 21:49:57 +0200

    VB::create section Hero

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Header(){
   return(
     <header className="flex items-center justify-between">
-      <Link className="text-red-500 font-semibold text-2xl" href="">ST PIZZA</Link>
+      <Link className="text-red-500 font-semibold text-2xl" href="/">ST PIZZA</Link>
       <nav className="flex items-center gap-8 text-gray-500 font-semibold">
         <Link href={''}>Home</Link>
         <Link href={''}>Menu</Link>
--- src/components/layout/Hero.jsx
+++ src/components/layout/Hero.jsx
@@ -1,14 +1,37 @@
+import Image from "next/image";
+import Righ from "../icons/Right";
+
 export default function Hero(){
   return(
-    <section>
-      <h1 className="text-4xl font-semibold">   
-        Everything<br />
-        is better<br />
-        with a&nbsp;
-        <span className="text-red-500">
+    <section className="block md:grid grid-cols-2 grid-cols-[.4fr .6fr] md:mt-4">
+      <div className="py-8 md:py-12">
+        <h1 className="text-4xl font-semibold">   
+          Everything<br />
+          is better<br />
+          with a&nbsp;
+          <span className="text-red-500">
             Pizza
-          </span></h1>
-      <p>Pizza is the missing piece that makes every day complete, a simple yet delicious joy in life</p>
+          </span>
+        </h1>
+        <p className="mt-4 text-gray-500 mb-6">
+          Pizza is the missing piece that makes every day complete, a simple yet delicious joy in life
+        </p>
+        <div className="flex gap-4 text-sm">
+          <button type="button" className="bg-red-500 flex justify-center uppercase items-center gap-2 text-white px-4 py-2 rounded-full">
+            Order now
+            <Righ/>
+
+          </button>
+          <button type="button" className="flex items-center border-0 gap-2 py-2 text-gray-600 font-semibold">
+            Learn more
+            <Righ/>
+          </button>
+        </div>
+      </div>
+
+      <div className="relative hidden md:block">
+        <Image src={'/pizza.png'} layout={"fill"} objectFit={'contain'} alt="pizza"/>
+      </div>
 
     </section>
   )
